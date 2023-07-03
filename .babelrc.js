d57c28f6e66ae310c2905e5a5e44c93ba466c907
hieua7k48 <longnv031120@gmail.com>
2023-07-03 11:16:55 +0700

    Cài đặt và cấu hình Prettier

--- .babelrc.js
+++ .babelrc.js
@@ -3,10 +3,8 @@ const plugins = [
         require.resolve('babel-plugin-module-resolver'),
         {
             alias: {
-                "@": "./src"
-            }
-        }
-
-    ]
-
+                '@': './src',
+            },
+        },
+    ],
 ];
